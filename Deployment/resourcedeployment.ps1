b3a0ef2ee6acdc6c90cbd13fe0664bafafc46d89
Harmanpreet Kaur <v-harmanpkau@microsoft.com>
2024-12-09 16:21:23 +0530

    edit 63

--- Deployment/resourcedeployment.ps1
+++ Deployment/resourcedeployment.ps1
@@ -209,7 +209,24 @@ function DisplayResult([pscustomobject]$jsonString) {
     Write-Host "* Azure Storage Account " -ForegroundColor Yellow -NoNewline; Write-Host "$storageAccountName" -ForegroundColor Green
     Write-Host "* Azure Cosmos DB " -ForegroundColor Yellow -NoNewline; Write-Host "$azcosmosDBName" -ForegroundColor Green
     Write-Host "* Azure App Configuration Endpoint " -ForegroundColor Yellow -NoNewline; Write-Host "$azappConfigEndpoint" -ForegroundColor Green
-    Write-Host "rg_name=$resourcegroupName" >> $env:GITHUB_ENV
+    #Write-Host "rg_name=$resourcegroupName" >> $env:GITHUB_ENV
+
+    try {
+
+    # Validate the value
+    if (-Not $resourcegroupName) {
+        throw "The resource group name is missing or invalid in the JSON object."
+    }
+
+    # Write the value to the GITHUB_ENV file
+    "$env:rg_name=$resourcegroupName" | Out-File -FilePath $env:GITHUB_ENV -Append -Encoding utf8
+
+    Write-Host "Successfully set the resource group name as an environment variable."
+    } catch {
+    # Handle errors
+    Write-Error "An error occurred while setting the resource group name: $_"
+    exit 1 # Exit the script with a failure code
+    }
 
 }
 
@@ -889,25 +906,6 @@ try {
     Write-Host "Don't forget to control the TPM rate for your GPT and Embedding Model in Azure Open AI Studio Deployments section." -ForegroundColor Red
     Write-Host "After controlling the TPM rate for your GPT and Embedding Model, let's start Data file import process with this command." -ForegroundColor Yellow
     Write-Host ".\uploadfiles.ps1 -EndpointUrl https://${fqdn}" -ForegroundColor Green
-
-try {
-    # Extract the resource group name dynamically from the JSON object
-    $rgName = $jsonString.properties.outputs.gs_resourcegroup_name.value
-
-    # Validate the value
-    if (-Not $rgName) {
-        throw "The resource group name is missing or invalid in the JSON object."
-    }
-
-    # Write the value to the GITHUB_ENV file
-    "$env:rg_name=$rgName" | Out-File -FilePath $env:GITHUB_ENV -Append -Encoding utf8
-
-    Write-Host "Successfully set the resource group name as an environment variable."
-} catch {
-    # Handle errors
-    Write-Error "An error occurred while setting the resource group name: $_"
-    exit 1 # Exit the script with a failure code
-}
 
 
 
