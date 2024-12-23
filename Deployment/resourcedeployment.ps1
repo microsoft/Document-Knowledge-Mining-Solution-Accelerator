e98f5e6df187d7be6b19080b45c880d17444e5be
Harmanpreet Kaur <v-harmanpkau@microsoft.com>
2024-12-23 16:15:08 +0530

    final commit heelo

--- Deployment/resourcedeployment.ps1
+++ Deployment/resourcedeployment.ps1
@@ -128,14 +128,17 @@ $email = $params.email
 
 function LoginAzure([string]$subscriptionID) {
         Write-Host "Log in to Azure.....`r`n" -ForegroundColor Yellow
-        if ($env:CI -eq "false"){
-        az login
-        }
-        else{
-            az login --service-principal `
+        if ($env:CI -eq "true"){
+      
+        az login --service-principal `
         --username $env:AZURE_CLIENT_ID `
         --password $env:AZURE_CLIENT_SECRET `
         --tenant $env:AZURE_TENANT_ID
+        write-host "CI deployment mode"
+        }
+        else{
+              az login
+        write-host "manual deployment mode"
         }
         az account set --subscription $subscriptionID
         Write-Host "Switched subscription to '$subscriptionID' `r`n" -ForegroundColor Yellow
