41556647476fc0f38afdc57748b71ce6375555ae
Bangarraju-Microsoft <v-golib@microsoft.com>
2025-07-09 15:25:37 +0530

    but fix #20379

--- App/frontend-app/src/components/documentViewer/dialogContentComponent.tsx
+++ App/frontend-app/src/components/documentViewer/dialogContentComponent.tsx
@@ -52,7 +52,7 @@ export function DialogContentComponent({
                 </div>
             )}
 
-            <div className="mb-4 h-96 overflow-y-auto">
+            <div className="mb-4 h-96 overflow-y-auto" style={{height:'auto'}}>
                 {allChunkTexts.map(
                     (item, index) =>
                         item && (
