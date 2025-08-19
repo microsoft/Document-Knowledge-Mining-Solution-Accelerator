a47d63ce5f3daf722622e934447b21e843939cf9
Ritesh <v-riteshmate@microsoft.com>
2025-08-19 18:06:39 +0530

    dkm-locator-updated

--- tests/e2e-test/pages/dkmPage.py
+++ tests/e2e-test/pages/dkmPage.py
@@ -9,7 +9,7 @@ from base.base import BasePage
 class DkmPage(BasePage):
     WELCOME_PAGE_TITLE = "(//div[@class='order-5 my-auto pb-3 text-lg font-semibold leading-tight text-white mt-3'])[1]"
     NEWTOPIC = "//button[normalize-space()='New Topic']"
-    Suggested_follow_up_questions = "body > div:nth-child(3) > div:nth-child(1) > main:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(6) > div:nth-child(3) > button:nth-child(2)"
+    Suggested_follow_up_questions = "body > div:nth-child(3) > div:nth-child(1) > main:nth-child(2) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(6) > div:nth-child(3) > button:nth-child(2)"
     SCROLL_DOWN = "//div[10]//div[2]//div[2]//i[1]//img[1]"
     ASK_QUESTION = (
         "//textarea[@placeholder='Ask a question or request (ctrl + enter to submit)']"
