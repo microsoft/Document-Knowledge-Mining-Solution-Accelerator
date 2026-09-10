a38c738573ef7403bfe1358d9787cbd9dd7122e8
Ayaz-Microsoft <v-ayazkhan@microsoft.com>
2026-09-10 09:35:23 +0530

    Refactor formatLabel function to improve label formatting logic

--- App/frontend-app/src/components/filter/filter.tsx
+++ App/frontend-app/src/components/filter/filter.tsx
@@ -42,8 +42,9 @@ interface FilterProps {
 
 const formatLabel = (label: string): string => {
     return label
-        .replace(/([A-Z])/g, ' $1')
         .replace(/_/g, ' ')
+        .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
+        .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2')
         .replace(/\s+/g, ' ')
         .trim()
         .replace(/^./, str => str.toUpperCase());
